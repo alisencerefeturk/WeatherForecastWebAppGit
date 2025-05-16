2f67d243f038602c7d13751c2acf87f6b8b4d209
alisencerefeturk <alisencerefeturk@hotmail.com>
2025-05-16 23:59:08 +0300

    login.html and register.html file added, register and login system added, project connected to sql database for storing user informations. general code readability improved. favorite city function added to homepage.

--- index.js
+++ index.js
@@ -3,73 +3,148 @@ require('dotenv').config();
 const express = require('express');
 const { accessSync } = require('fs');
 const app = express();
+const bcrypt = require('bcrypt');
+const { error } = require('console');
+const sql = require('mssql');
 
 const PORT = 3000;
 
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Sunucu başarıyla çalışıyor')
-})
+  res.send('Sunucu başarıyla çalışıyor');
+});
 
 app.listen(PORT, () => {
-    console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
-})
+  console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
 
-app.post('/api/weather', async(req, res) =>{
-    const city = req.body.city;
-    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`
-    try {
-        const response = await axios.get(geoURL);
-        const geoData = response.data;
-        const lat = geoData[0].lat;
-        const lon = geoData[0].lon;
-        // console.log("Geo API'den gelen veri:", geoData); //Geo API verisi kontrolü için
+app.post('/api/weather', async (req, res) => {
+  const city = req.body.city;
+  const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`;
 
-        const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
-        const weatherResponse = await axios.get(weatherURL);
-        const weatherData = weatherResponse.data
-        const prompt = `City:${city}
-        Temperature:${weatherData.current.temp}
-        Humidity:${weatherData.current.humidity}
-        Wind speed:${weatherData.current.wind_speed}
-        Weather:${weatherData.current.weather[0].main}
-        Dil tamamen türkçe olsun.
-        `
-        const weatherIconCode = `${weatherData.current.weather[0].icon}`;
-        const weatherIconURL = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
-        // console.log(`Şehir bilgisi vs:${weatherData}`); // Tüm bilgileri ve apileri kontrol için
+  try {
+    const response = await axios.get(geoURL);
+    const geoData = response.data;
+    const lat = geoData[0].lat;
+    const lon = geoData[0].lon;
 
-        const headers = {
-            "Content-Type": "application/json",
-        };
-        
-        const geminiBody = {
-            contents: [
-                {
-                    parts: [
-                        { text: prompt }
-                    ]
-                }
-            ]
-        };
-        const geminiURL = `https://generativelanguage.googleapis.com/v1beta/tunedModels/geminiweatherdataset4999tr-9uinx7zoq788:generateContent?key=${process.env.GEMINI_API_KEY}`;
-        const geminiResponse = await axios.post(geminiURL, geminiBody, { headers });
-        const aiText = geminiResponse.data?.candidates?.[0]?.content?.parts?.[0]?.text || "Yorum alınamadı.";
-        
-        res.json({
-            geo: geoData,
-            weather: weatherData,
-            aiComment: aiText,
-            weatherIcon: weatherIconURL
-          });
-        // console.log(JSON.stringify(geminiResponse.data, null, 2));
-          
+    const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`;
+    const weatherResponse = await axios.get(weatherURL);
+    const weatherData = weatherResponse.data;
 
-    } catch(error) {
-        console.error("Hata:", error);
-        res.status(500).json({ error: "Hata alındı" });
+    const prompt = `City:${city}\nTemperature:${weatherData.current.temp}\nHumidity:${weatherData.current.humidity}\nWind speed:${weatherData.current.wind_speed}\nWeather:${weatherData.current.weather[0].main}\nDil tamamen türkçe olsun.`;
+
+    const weatherIconCode = `${weatherData.current.weather[0].icon}`;
+    const weatherIconURL = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
+
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    const geminiBody = {
+      contents: [
+        {
+          parts: [{ text: prompt }]
+        }
+      ]
+    };
+
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/tunedModels/geminiweatherdataset4999tr-9uinx7zoq788:generateContent?key=${process.env.GEMINI_API_KEY}`;
+    const geminiResponse = await axios.post(geminiURL, geminiBody, { headers });
+    const aiText = geminiResponse.data?.candidates?.[0]?.content?.parts?.[0]?.text || 'Yorum alınamadı.';
+
+    res.json({
+      geo: geoData,
+      weather: weatherData,
+      aiComment: aiText,
+      weatherIcon: weatherIconURL
+    });
+  } catch (error) {
+    console.error('Hata:', error);
+    res.status(500).json({ error: 'Hata alındı' });
+  }
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  console.log('Formdan gelen kullanıcı adı: ', username);
+  console.log('Formdan gelen şifre:', password);
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log('şifrelenmiş hali:', hashedPassword);
+
+    const pool = await sql.connect(dbConfig);
+    await pool
+      .request()
+      .input('username', sql.NVarChar, username)
+      .input('password', sql.NVarChar, hashedPassword)
+      .query('INSERT INTO Users (username, password) VALUES (@username, @password)');
+
+    res.send(`<h3>Kayıt başarılı! <a href="/login.html">Giriş yap</a></h3>`);
+  } catch (err) {
+    console.error('Hata', err);
+    res.status(500).send('Bir hata oluştu.');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input('username', sql.NVarChar, username)
+      .query('SELECT * FROM Users WHERE username = @username');
+
+    const user = result.recordset[0];
+
+    if (!user) {
+      return res.send('Kullanıcı bulunamadı');
     }
-})
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.send('Şifre hatalı');
+    }
+    res.send(`Giriş başarılı! Hoş geldin, ${user.username}`);
+
+  } catch (err) {
+    console.error('Girişte hata:', err);
+    res.status(500).send('Bir hata oluştu.');
+  }
+});
+
+app.post('/favorite', async (req, res) => {
+  const { city } = req.body;
+  const username = 'sencer'; // Not: Bu geçici. Gerçek uygulamada giriş yapmış kullanıcıdan alınmalı.
+
+  try {
+    const pool = await sql.connect(dbConfig);
+    await pool
+      .request()
+      .input('username', sql.NVarChar, username)
+      .input('city', sql.NVarChar, city)
+      .query('UPDATE Users SET favoriteCity = @city WHERE username = @username');
+      res.send('Favori şehir kaydedildi!');
+  } catch (err) {
+    console.error('Favori şehir kaydında hata:', err);
+    res.status(500).send('Bir hata oluştu.');
+  }
+  
+});
--- public/scripts.js
+++ public/scripts.js
@@ -19,7 +19,7 @@ async function sendCityToServer() {
         document.getElementById('weatherAiResult').textContent = data.aiComment;
         document.getElementById('weatherIcon').src = data.weatherIcon;
         console.log("Sunucudan gelen:", data);
-        resultBoxIco.style.display = 'block';
+        resultBoxIcoDiv.style.display = 'block';
     } catch (error) {
         console.error("Hata:", error);
         document.getElementById('weatherAiResult').textContent = "Bir hata oluştu. Lütfen tekrar deneyin.";
@@ -28,6 +28,7 @@ async function sendCityToServer() {
         button.disabled = false;
         button.classList.remove('loading');
         buttonText.textContent = 'Tahmin Al';
+        addFavCitybutton.style.display = 'block';
     }
 }
 
@@ -36,9 +37,10 @@ document.getElementById('getWeatherBtn').addEventListener('click', function(even
     sendCityToServer();
 });
 
+
 const resultBox = document.getElementById('weather-ai');
-const resultBoxIcoDiv = document.getElementById('weatherIconDiv');
-const resultBoxIco = document.getElementById('weatherIcon');
+const resultBoxIcoDiv = document.getElementById('weather-icon-div');
+const addFavCitybutton = document.getElementById('add-fav-button')
 
 resultBox.classList.remove('visible');
 resultBoxIcoDiv.classList.remove('visible');
@@ -47,3 +49,23 @@ setTimeout(() => {
   resultBox.classList.add('visible');
   resultBoxIcoDiv.classList.add('visible');
 }, 10);
+
+document.getElementById('weather-form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const city = document.getElementById('city').value;
+
+  const response = await fetch('/favorite', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ city })
+  });
+
+  
+const data = await response.text();
+const msgBox = document.getElementById('add-fav-confirm');
+msgBox.textContent = data;
+msgBox.style.display = 'block';
+setTimeout(() => {
+  msgBox.style.display = 'none';
+}, 3000);
+});
